6f2ea48a91a077447543d133384c99ed72c6ab3e
simuhunluo <simuhunluo@outlook.com>
2024-01-12 01:47:03 +0800

    镜像构建脚本(阿里云)

--- build-debrief-image.sh
+++ build-debrief-image.sh
@@ -5,4 +5,4 @@ docker build -t debrief-server .
 docker tag debrief-server:latest registry.cn-beijing.aliyuncs.com/simuhunluo/debrief-server:0112
 docker push registry.cn-beijing.aliyuncs.com/simuhunluo/debrief-server:0112
 # docker pull simuhunluo/debrief-server:1204
-#
+# 1
