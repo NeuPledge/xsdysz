61ff9765808339b6fa40cef2035053af124d0dc9
simuhunluo <simuhunluo@outlook.com>
2024-03-21 00:25:34 +0800

    【debrief】述责统计表数据修正

--- build-debrief-image.sh
+++ build-debrief-image.sh
@@ -2,7 +2,7 @@
 cd ./yudao-server
 docker build -t debrief-server .
 
-docker tag debrief-server:latest registry.cn-beijing.aliyuncs.com/simuhunluo/debrief-server:0112
-docker push registry.cn-beijing.aliyuncs.com/simuhunluo/debrief-server:0112
+docker tag debrief-server:latest registry.cn-beijing.aliyuncs.com/simuhunluo/debrief-server:0321
+docker push registry.cn-beijing.aliyuncs.com/simuhunluo/debrief-server:0321
 # docker pull simuhunluo/debrief-server:1204
 # 1
