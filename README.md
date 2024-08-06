002050018c1ff2c24b8520f908fbb43d933eaa1f
simuhunluo <simuhunluo@outlook.com>
2024-08-06 23:52:25 +0800

    【game】后端可以跑通

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-local 本地开发用，连的是开发数据库
-test 本地测试用，连的是线上库
-dev 线上用，连的是线上库
+
+# 关于环境区分
+dev 本地开发用，连的是开发库 pandacloudgame
+test 本地测试用，连的是线上库 pandacloudgame_prod
+prod 线上用，连的是线上库 pandacloudgame_prod
+
+#  启动必须中间件
+### redis
+本地dev: 119.101.148.189
+本地test: 119.101.148.189
+prod: 127.0.0.1 (同机器)
+
+
--- yudao-module-game/doc/app.sql
+++ yudao-module-game/doc/app.sql
@@ -1,3 +1,16 @@
+CREATE TABLE `game_uid_pool`
+(
+    `id`          bigint(11) NOT NULL COMMENT '自增主键',
+    `creator`     varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '创建者',
+    `create_time` datetime     NOT NULL                                        DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updater`     varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '更新者',
+    `update_time` datetime     NOT NULL                                        DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted`     bit(1)       NOT NULL                                        DEFAULT b'0' COMMENT '是否删除',
+    `tenant_id`   bigint(20) NOT NULL DEFAULT '0' COMMENT '租户编号',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户id池子';
+
+
 CREATE TABLE `game_idc_host`
 (
     `id`          bigint(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
